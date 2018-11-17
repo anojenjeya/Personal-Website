3dc7a5da69c58729b614a24d6e5404e1561da880
Anojen <anojenjeya@gmail.com>
2018-11-17 15:07:30 -0500

    Styling, added project pages template
    - added tooltips
    - footer
    - parallax
    - more icons
    - updated resume
    - js for scrolling to top

--- js/main.js
+++ js/main.js
@@ -8,3 +8,27 @@ $(window).scroll(function() {
     }
   });
 });
+
+$(document).on('click', 'a[href^="#"]', function(event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 800);
+});
+
+
+
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scrollTopBtn').fadeIn(); 
+        } else { 
+            $('#scrollTopBtn').fadeOut(); 
+        } 
+    }); 
+    $('#scrollTopBtn').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
